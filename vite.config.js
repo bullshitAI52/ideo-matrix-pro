ba803a54ee9cc20d601fd820239281c7e57a7391
zwm5646 <wearverbertha@gmail.com>
2025-12-03 12:33:07 +0800

    Fix vite config: set root to src

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    root: "src",
     clearScreen: false,
     server: {
         port: 1420,
@@ -8,6 +9,8 @@ export default defineConfig({
     },
     envPrefix: ["VITE_", "TAURI_"],
     build: {
+        outDir: "../dist",
+        emptyOutDir: true,
         target: ["es2021", "chrome100", "safari13"],
         minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
         sourcemap: !!process.env.TAURI_DEBUG,
